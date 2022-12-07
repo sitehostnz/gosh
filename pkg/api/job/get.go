174a5b94d56e2c3189d6e40dd0d1e12b08f3d037
Tommy Ngo <tommy@sitehost.co.nz>
2022-12-08 09:02:55 +1300

    Fix job API endpoint and update CHANGELOG

--- pkg/api/job/get.go
+++ pkg/api/job/get.go
@@ -9,8 +9,6 @@ import (
 
 // Get information about a job.
 func (s *Client) Get(ctx context.Context, request GetRequest) (response GetResponse, err error) {
-	u := "job/get.json"
-
 	keys := []string{
 		"job_id",
 		"type",
@@ -20,7 +18,9 @@ func (s *Client) Get(ctx context.Context, request GetRequest) (response GetRespo
 	values.Add("job_id", request.JobID)
 	values.Add("type", request.Type)
 
-	req, err := s.client.NewRequest("GET", u, utils.Encode(values, keys))
+	u := "job/get.json?" + utils.Encode(values, keys)
+
+	req, err := s.client.NewRequest("GET", u, "")
 	if err != nil {
 		return response, err
 	}
