1267a2a03384ac6971e2637038b9a4da07cd1b23
John Lennard <john@yakmoo.se>
2023-03-03 16:11:58 +1300

    add in helpers for easing dns record behaviours on the api, these could go in terraform. but feels like addressing api quirks should live with the api

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -5,6 +5,8 @@ import (
 	"testing"
 )
 
+const exampleDotCom = "example.com."
+
 func TestEncode(t *testing.T) {
 	t.Parallel()
 	v := url.Values{}
@@ -14,6 +16,94 @@ func TestEncode(t *testing.T) {
 	keys := []string{"key1", "key2"}
 	expected := "key1=value1&key2=value2"
 	result := Encode(v, keys)
+	if result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestConstructFqdnWithAtSymbolAndName(t *testing.T) {
+	t.Parallel()
+
+	result := ConstructFqdn("@", "example.com")
+
+	if result != exampleDotCom {
+		t.Errorf("Expected %s but got %s", exampleDotCom, result)
+	}
+}
+
+func TestConstructFqdnWithDotSymbolAndName(t *testing.T) {
+	t.Parallel()
+	result := ConstructFqdn(".", "example.com")
+
+	if result != exampleDotCom {
+		t.Errorf("Expected %s but got %s", exampleDotCom, result)
+	}
+}
+
+func TestConstructFqdnWithAtSymbolAndNameAndTrailingDot(t *testing.T) {
+	t.Parallel()
+	result := ConstructFqdn("@", exampleDotCom)
+
+	if result != exampleDotCom {
+		t.Errorf("Expected %s but got %s", exampleDotCom, result)
+	}
+}
+
+func TestConstructFqdnWithSubDomain(t *testing.T) {
+	t.Parallel()
+	expected := "www.example.com."
+	result := ConstructFqdn("www", "example.com")
+
+	if result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestConstructFqdnWithFdqn(t *testing.T) {
+	t.Parallel()
+	expected := exampleDotCom
+	result := ConstructFqdn(exampleDotCom, "example.com")
+
+	if result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestConstructFqdnWithFdqnAndSubDomain(t *testing.T) {
+	t.Parallel()
+	expected := "www.example.com."
+	result := ConstructFqdn("www.example.com.", "example.com")
+
+	if result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestConstructFqdnWithNotFdqnAndSubDomain(t *testing.T) {
+	t.Parallel()
+	expected := "www.example.com.example.com."
+	result := ConstructFqdn("www.example.com", "example.com")
+
+	if result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestDeconstructFqdnApex(t *testing.T) {
+	t.Parallel()
+	expected := "@"
+	result := DeconstructFqdn(exampleDotCom, exampleDotCom)
+
+	if result != expected {
+		t.Errorf("Expected %s but got %s", expected, result)
+	}
+}
+
+func TestDeconstructFqdnSubDomain(t *testing.T) {
+	t.Parallel()
+	expected := "www"
+	result := DeconstructFqdn("www.example.com", "example.com")
+
 	if result != expected {
 		t.Errorf("Expected %s but got %s", expected, result)
 	}
