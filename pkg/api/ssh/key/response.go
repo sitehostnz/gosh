a080dbc1929a5e6c7657ae644ccde62429121c6a
John Lennard <john@yakmoo.se>
2023-03-13 12:48:12 +1300

    linty fresh and clean

--- pkg/api/ssh/key/response.go
+++ pkg/api/ssh/key/response.go
@@ -11,4 +11,31 @@ type (
 		} `json:"return"`
 		models.APIResponse
 	}
+	// GetResponse is the return from a GetRequest.
+	GetResponse struct {
+		Return struct {
+			ID          string `json:"id"`
+			ClientID    string `json:"client_id"`
+			Label       string `json:"label"`
+			Content     string `json:"content"`
+			DateAdded   string `json:"date_added"`
+			DateUpdated string `json:"date_updated"`
+		} `json:"return"`
+		models.APIResponse
+	}
+	// AddResponse is the return from an AddRequest.
+	AddResponse struct {
+		Return struct {
+			KeyID string `json:"key_id"`
+		} `json:"return"`
+		models.APIResponse
+	}
+	// UpdateResponse is the return from an UpdateRequest.
+	UpdateResponse struct {
+		models.APIResponse
+	}
+	// RemoveResponse is the return from an RemoveRequest.
+	RemoveResponse struct {
+		models.APIResponse
+	}
 )
---
+++ pkg/api/ssh/key/update.go
@@ -0,0 +1,36 @@
+package key
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/sitehostnz/gosh/pkg/utils"
+)
+
+// Update updates the key with the given ID.
+func (s *Client) Update(ctx context.Context, request UpdateRequest) (response UpdateResponse, err error) {
+	uri := "ssh/key/update.json"
+	keys := []string{
+		"client_id",
+		"key_id",
+		"label",
+		"content",
+	}
+
+	values := url.Values{}
+	values.Add("client_id", s.client.ClientID)
+	values.Add("key_id", request.ID)
+	values.Add("label", request.Label)
+	values.Add("content", request.Content)
+
+	req, err := s.client.NewRequest("POST", uri, utils.Encode(values, keys))
+	if err != nil {
+		return response, err
+	}
+
+	if err := s.client.Do(ctx, req, &response); err != nil {
+		return response, err
+	}
+
+	return response, nil
+}
